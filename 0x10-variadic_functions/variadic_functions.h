d1275af1e06d4021d25a7b0ade064a71da7226dd
Pcosby5 <pcosby50@gmail.com>
2023-08-18 00:34:28 +0000

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,6 +7,15 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 #include<stdarg.h>
 
-
+/**
+ * struct print - print various format types
+ * @idt: char identifier
+ * @fptr: function pointer
+ */
+typedef struct print
+{
+	char *idt;
+	void (*fptr)(va_list);
+}t_print;
 
 #endif
