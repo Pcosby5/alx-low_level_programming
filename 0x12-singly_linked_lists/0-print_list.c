d5fb69c6d262b22b96c08cebd0e91405fedd66b9
Pcosby5 <pcosby50@gmail.com>
2023-08-24 18:19:01 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,27 +4,6 @@
 #include <stdio.h>
 
 /**
- * _strlen - returns the length of string
- * @str: string who is about to be checked
- * Return: length of string
- */
-
-int _strlen(char *str)
-{
-	int j = 0;
-
-	if (str == NULL)
-	{
-		return (0);
-	}
-	while (str++)
-	{
-		j++;
-	}
-	return (j);
-}
-
-/**
  * print_list - prints all element of a list
  * @h: pointer to head of the list
  * Return: number of nodes
@@ -34,19 +13,14 @@ size_t print_list(const list_t *h)
 {
 	size_t p = 0;
 
-	if (h == NULL)
-		return (0);
-
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-
-		p++;
-
 		h = h->next;
+		p++;
 	}
 	return (p);
 }
