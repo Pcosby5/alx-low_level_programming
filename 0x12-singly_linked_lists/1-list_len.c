50eac28c63521108e96d771b8a245a7ce94dd7ee
Pcosby5 <pcosby50@gmail.com>
2023-08-24 18:24:14 +0000

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * list_len - returns the length of the pointer
- * @h: pointer to head of the list
+ * list_len - returns the number of elements
+ * @h: pointer to the list
  * Return: number of elements in a node
  */
 
 size_t list_len(const list_t *h)
 {
 	size_t p = 0;
-
-	if (h == NULL)
-		return (0);
 
 	while (h != NULL)
 	{
