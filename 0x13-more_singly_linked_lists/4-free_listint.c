8cd17f4799fd7bafa86800435da7ec4547e34d5f
Pcosby5 <pcosby50@gmail.com>
2023-08-29 00:48:59 +0000

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a linked list
+ * @head: pointer to be freed
+ */
+
+void free_listint(listint_t *head)
+{
+	if (head != NULL)
+	{
+		free_listint(head->next);
+		free(head);
+	}
+}
