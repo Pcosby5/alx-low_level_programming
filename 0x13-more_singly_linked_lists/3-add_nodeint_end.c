4791be332efc0f8c35aeade8fd6a9ba443e3eb52
Pcosby5 <pcosby50@gmail.com>
2023-08-29 02:23:46 +0000

    committed changes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newnode;
 	listint_t *temp = *head;
 
+	if (head == NULL)
+		return (NULL);
+
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
