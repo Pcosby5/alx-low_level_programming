38bfe21dfd98cc18341ae6bc68477754590a395e
Pcosby5 <pcosby50@gmail.com>
2023-08-29 02:15:54 +0000

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,38 +8,27 @@
  * Return: address of new node
  */
 
-
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *newnode;
-	listint_t *temp;
-
-	if (head == NULL)
-		return (NULL);
-
-	temp = *head;
-
-	if (temp != NULL)
-
-		while (temp->next != NULL)
-			temp = temp->next;
+	listint_t *temp = *head;
 
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 
 	newnode->n = n;
+	newnode->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = newnode;
-		newnode->next = NULL;
+		return (newnode);
 	}
 
-	else
-	{
-		temp->next = newnode;
-		newnode->next = NULL;
-	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = newnode;
+
 	return (newnode);
 }
