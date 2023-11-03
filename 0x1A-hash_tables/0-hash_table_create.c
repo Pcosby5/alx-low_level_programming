9a301f196f416a282ee7556e35558af9deae1864
Pcosby5 <pcosby50@gmail.com>
2023-11-03 14:49:39 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,37 +8,36 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    /* Declare a pointer to a hash table structure */
-    hash_table_t *table;
+	/* Declare a pointer to a hash table structure */
+	hash_table_t *table;
 
-    /* Declare a pointer to an array of hash nodes */
-    hash_node_t **array;
+	/* Declare a pointer to an array of hash nodes */
+	hash_node_t **array;
 
-    /* Declare a variable for iterating through the array */
-    unsigned long int i;
+	/* Declare a variable for iterating through the array */
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    table = malloc(sizeof(hash_table_t));
+	/* Allocate memory for the hash table structure */
+	table = malloc(sizeof(hash_table_t));
 
-    /* Check if memory allocation failed */
-    if (table == NULL)
-        return (NULL);
+	/* Check if memory allocation failed */
+	if (table == NULL)
+		return (NULL);
 
-    /* Allocate memory for an array of hash nodes */
-    array = malloc(sizeof(hash_node_t *) * size);
+	/* Allocate memory for an array of hash nodes */
+	array = malloc(sizeof(hash_node_t *) * size);
 
-    /* Check if memory allocation failed */
-    if (array == NULL)
-        return (NULL);
+	/* Check if memory allocation failed */
+	if (array == NULL)
+		return (NULL);
 
-    /* Initialize each element in the array to NULL */
-    for (i = 0; i < size; i++)
-        array[i] = NULL;
+	/* Initialize each element in the array to NULL */
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
-    /* Assign the array and size to the hash table structure */
-    table->array = array;
-    table->size = size;
-
-    /* Return a pointer to the created hash table */
-    return (table);
+	/* Assign the array and size to the hash table structure */
+	table->array = array;
+	table->size = size;
+	/* Return a pointer to the created hash table */
+	return (table);
 }
