d2ef649a64ed15a1e8d056baf406f8049ffe6591
Pcosby5 <pcosby50@gmail.com>
2023-11-03 15:41:11 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,31 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table with a given size
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * @size: size of the hash table
- *
- * Return: the created hash table, or NULL if function fails
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table; /* Declare a pointer to a hash table */
-	hash_node_t **array; /* Declare a pointer to an array of hash nodes */
-	unsigned long int i; /* Declare an unsigned long integer variable */
+	hash_table_t *ht;
+	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t)); /* Allocate memory for the hash table */
-	if (table == NULL)					  /* Check if allocation was successful */
-		return (NULL);					  /* Return NULL if allocation failed */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size); /* Allocate memory for the array of hash nodes */
-	if (array == NULL)							  /* Check if allocation was successful */
-		return (NULL);							  /* Return NULL if allocation failed */
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-	for (i = 0; i < size; i++) /* Iterate through the array of hash nodes */
-		array[i] = NULL;	   /* Initialize each element to NULL */
-
-	table->array = array; /* Assign the array to the hash table */
-	table->size = size;	  /* Assign the size to the hash table */
-
-	return (table); /* Return the created hash table */
+	return (ht);
 }
