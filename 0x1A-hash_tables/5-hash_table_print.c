aa364dc69663dce321a56fc511d347379703ed22
Pcosby5 <pcosby50@gmail.com>
2023-11-03 15:36:31 +0000

    5-hash_table_print.c

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,33 +1,32 @@
-#include "hash_tables.h"
 
+#include "hash_tables.h"
 /**
- * hash_table_print - prints the content of a hash table
- * @ht: the hash table to print
+ * hash_table_print - prints hash table
+ * @ht: hash table to print
+ * Return: Nothing.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	hash_node_t *tmp = NULL;  /* Temporarily stores the current node */
-	unsigned long int i = 0, a = 0;  /* Loop counters for hash array and node list */
-	char *sep_btn = ", ";  /* Separator between key-value pairs */
-	char *sep_bef = "";  /* Separator before the first key-value pair */
+	hash_node_t *temp = NULL;
+	unsigned long int i = 0, a = 0;
+	char *new = ", ", *space = "";
 
 	if (ht == NULL)
-		return;  /* If the hash table is NULL, do nothing */
-
+		return;
 	if (ht != NULL)
 	{
-		printf("{");  /* Print the opening curly brace */
+		printf("{");
 		for (; i < ht->size; i++)
 		{
-			tmp = ht->array[i];  /* Get the current node in the array */
-			for (; tmp != NULL; a++)
+			temp = ht->array[i];
+			for (; temp != NULL; a++)
 			{
-				printf("%s", sep_bef);  /* Print the separator */
-				printf("'%s': '%s'", tmp->key, tmp->value);  /* Print the key-value pair */
-				sep_bef = sep_btn;  /* Set the separator to ',' for subsequent pairs */
-				tmp = tmp->next;  /* Move to the next node in the list */
+				printf("%s", space);
+				printf("'%s': '%s'", temp->key, temp->value);
+				space = new;
+				temp = temp->next;
 			}
 		}
-		printf("}\n");  /* Print the closing curly brace and a newline character */
+		printf("}\n");
 	}
 }
