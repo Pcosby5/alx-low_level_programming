da6b18dcf84bf8ab7a77ca4ef7502d5384421e09
Pcosby5 <pcosby50@gmail.com>
2023-11-03 15:15:55 +0000

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,24 +20,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	/* Allocate memory for the hash table structure */
 	table = malloc(sizeof(hash_table_t));
 
-	/* Check if memory allocation failed */
-	if (table == NULL)
-		return (NULL);
-
-	/* Allocate memory for an array of hash nodes */
-	array = malloc(sizeof(hash_node_t *) * size);
-
-	/* Check if memory allocation failed */
-	if (array == NULL)
-		return (NULL);
-
-	/* Initialize each element in the array to NULL */
 	for (i = 0; i < size; i++)
 		array[i] = NULL;
 
 	/* Assign the array and size to the hash table structure */
 	table->array = array;
 	table->size = size;
+
 	/* Return a pointer to the created hash table */
 	return (table);
 }
