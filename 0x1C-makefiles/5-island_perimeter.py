2a9fb217a85672e35f9d227e9170637670017ee6
Pcosby5 <pcosby50@gmail.com>
2023-12-20 22:14:01 +0100

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,47 @@
 #!/usr/bin/python3
+"""Module that calculates the perimeter of an island in a grid."""
 
+
+# Function to count the water portion adjacent to a given cell in the grid
+def num_water_portion(grid, i, j):
+    """Returns the number of adjacent water portions to a cell in a grid."""
+
+    # Init a var to count the number of adjacent water portions
+    num = 0
+
+    # Check if the cell has water in the up dir or is at the edge
+    if i <= 0 or not grid[i - 1][j]:
+        num += 1
+
+    # Check if the cell has water portion in the left dir or is at the edge
+    if j <= 0 or not grid[i][j - 1]:
+        num += 1
+
+    # Check if the cell has water portion in the right dir or is at the edge
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        num += 1
+
+    # Check if the cell has water portion in the down dir or is at the edge
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        num += 1
+
+    return num
+
+
+# Function to calculate the perimeter of the island in the grid
 def island_perimeter(grid):
-    """
-    Calculates the perimeter of the island described in the grid.
+    """Returns the perimeter of the island in grid."""
 
-    Args:
-    - grid (List[List[int]]): Grid representing the island
-
-    Returns:
-    - int: Perimeter of the island
-    """
-
-    if not grid:
-        return 0
-
+    # Initialize the perimeter to zero
     perimeter = 0
-    rows, columns = len(grid), len(grid[0])
 
-    for i in range(rows):
-        for j in range(columns):
-            if grid[i][j] == 1:
-                perimeter += 4
-
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+    # Loop through each row in the grid
+    for i in range(len(grid)):
+        # Loop through each cell in the row
+        for j in range(len(grid[i])):
+            # Check if the cell represents land (1)
+            if grid[i][j]:
+                # Call the num_water_portion fnx and + the count to perimeter
+                perimeter += num_water_portion(grid, i, j)
 
     return perimeter
-
